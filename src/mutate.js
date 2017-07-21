c80187dfae05419d48ef7aa6c3924eccb3d2414c
Jonathan King <jonathan.j.king@gmail.com>
2017-07-21 12:23:10 +0100

    Flow fixes

--- src/mutate.js
+++ src/mutate.js
@@ -38,6 +38,34 @@ type RelayMutationConfig = (
     |}
 );
 
+declare class MutationPromise<+R> {
+    constructor(callback: (
+      resolve: (result: MutationPromise<R> | R) => void,
+      reject:  (error: any) => void
+    ) => mixed): void;
+
+    then<U>(
+      onFulfill?: (value: R) => MutationPromise<U> | U,
+      onReject?: (error: any) => MutationPromise<U> | U
+    ): MutationPromise<U>;
+
+    catch<U>(
+      onReject?: (error: any) => MutationPromise<U> | U
+    ): MutationPromise<R | U>;
+
+    onUploadProgress<T>(
+        cb: (any) => void
+    ): MutationPromise<T>;
+
+    abort: () => void;
+
+    static resolve<T>(object: MutationPromise<T> | T): MutationPromise<T>;
+    static reject<T>(error?: any): MutationPromise<T>;
+    static all<Elem, T:Iterable<Elem>>(promises: T): MutationPromise<$TupleMap<T, typeof $await>>;
+    static race<T, Elem: MutationPromise<T> | T>(promises: Array<Elem>): MutationPromise<T>;
+};
+
+export type Mutate = (config: MutationConfig) => MutationPromise<*>;
 
 export type MutationConfig = {
     query: Relay.QL,
@@ -47,16 +75,8 @@ export type MutationConfig = {
     configs?: RelayMutationConfig[]
 };
 
-export type MutationPromise = {
-    abort: () => void,
-    onUploadProgress: (cb: ProgressCallback) => MutationPromise,
-    then: (any) => MutationPromise,
-    catch: (any) => void
-};
-export type Mutate = (config: MutationConfig) => MutationPromise;
 
-
-export default function mutate(env: Relay.Environment, config: MutationConfig): MutationPromise {
+export default function mutate(env: Relay.Environment, config: MutationConfig): MutationPromise<*> {
     const {query, variables, files = null, optimisticResponse, configs = []} = config;
 
     var mutation;
